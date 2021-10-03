f1df1aadd1f3c329ac8a42d2f8a864e741e69856
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-10-03 20:32:59 -0300

    Criação models animal

---
+++ Models/Animal/Animal.cs
@@ -0,0 +1,32 @@
+﻿using SejaHeroi.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SejaHeroi.Models.Animal
+{
+    [Table("Animal")]
+    public class Animal
+    {
+        [Key]
+        public Guid AnimalID { get; set; }
+        public Guid UsuarioID { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+        public PorteAnimal TamanhoAnimal { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+        public Racas RacaAnimal { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+        public int Idade { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+        public Situacao Situacao { get; set; }
+        public string Resenha { get; set; }
+        public byte[] Foto { get; set; }
+
+        public bool StatusVacina { get; set; }
+    }
+}
