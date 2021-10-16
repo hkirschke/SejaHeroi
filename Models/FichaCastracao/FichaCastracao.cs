19a8ab14496d80cfb0b4f52981cdb6859d30c5dc
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-10-16 09:46:34 -0300

    Criação de models
    Model de ficha de castração e equipe veterinario

---
+++ Models/FichaCastracao/FichaCastracao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SejaHeroi.Models
+{
+    [Table("FichaCastracao")]
+    public class FichaCastracao
+    {
+        [Key]
+        public Guid CastracaoID { get; set; }
+        [DataType(DataType.DateTime, ErrorMessage = "Somente datas permitidas")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddThh:mm:ss}", ApplyFormatInEditMode = true)] 
+        public DateTime? DataEntrada { get; set; } 
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddThh:mm}", ApplyFormatInEditMode = true)] 
+        public DateTime? DataSaida { get; set; }
+        public Guid UsuarioID { get; set; }
+        public Guid AnimalID { get; set; }
+        public Guid EquipeVeterinarioID { get; set; }
+    }
+}
---
+++ Models/FichaCastracao/FichaCastracaoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SejaHeroi.Models
+{
+    public class FichaCastracaoViewModel
+    {
+        public Guid CastracaoID { get; set; }
+        public string DataEntrada { get; set; }
+        public string DataSaida { get; set; }
+        public string NomeAnimal { get; set; }
+        public Guid AnimalID { get; set; }
+        public Guid UsuarioID { get; set; }
+        public Guid EquipeVeterinarioID { get; set; }
+        public string NomeEquipe { get; set; }
+    }
+}
