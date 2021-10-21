4b9477fdc36d52474c94a9648cc6dd468f3ab080
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-10-20 23:20:02 -0300

    Ajuste basecoontroller

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,60 +1,63 @@
-﻿using SejaHeroi.Data; 
+﻿using SejaHeroi.Data;
+using SejaHeroi.DataUtil;
 using SejaHeroi.Extensions;
 using System.Globalization;
 using System.Web.Mvc;
 
 namespace SejaHeroi.Controllers
 {
-    public class BaseController : Controller
+  public class BaseController : Controller
+  {
+    protected readonly SejaHeroiContext _SejaHeroiContext = new SejaHeroiContext();
+    protected readonly SejaHeroiContextAnimalUtil _SejaHeroiContextAnimalUtil = new SejaHeroiContextAnimalUtil();
+    protected readonly SejaHeroiContextFichaUtil _SejaHeroiContextFichaUtil = new SejaHeroiContextFichaUtil();
+    protected readonly CultureInfo CultureInfo = CultureInfo.CreateSpecificCulture("pt-BR");
+
+    /// <summary>
+    /// {} realizado com sucesso!
+    /// </summary>
+    protected const string MensgemSucessoPadrao = "{0} com sucesso!";
+    /// <summary>
+    /// Erro ao efetuar \n{0}!
+    /// </summary>
+    protected const string MensgemErroPadrao = "Erro ao efetuar \n{0}!";
+    /// <summary>
+    /// Atenção! \n{0}
+    /// </summary>
+    protected const string MensgemAvisoPadrao = "Atenção! \n{0}";
+
+
+    // GET: Base
+    [AllowAnonymous]
+    public virtual ActionResult Index()
     {
-        protected readonly SejaHeroiContext _SejaHeroiContext = new SejaHeroiContext();
-        protected readonly CultureInfo CultureInfo = CultureInfo.CreateSpecificCulture("pt-BR");
-
-        /// <summary>
-        /// {} realizado com sucesso!
-        /// </summary>
-        protected const string MensgemSucessoPadrao = "{0} com sucesso!";
-        /// <summary>
-        /// Erro ao efetuar \n{0}!
-        /// </summary>
-        protected const string MensgemErroPadrao = "Erro ao efetuar \n{0}!";
-        /// <summary>
-        /// Atenção! \n{0}
-        /// </summary>
-        protected const string MensgemAvisoPadrao = "Atenção! \n{0}";
-
-
-        // GET: Base
-        [AllowAnonymous]
-        public virtual ActionResult Index()
-        {
-            return View();
-        }
-        /// <summary>
-        /// {0} realizado com sucesso!
-        /// </summary>
-        public void AddNotificacaoSucesso(string acao)
-        {
-            this.AddNotification(string.Format(MensgemSucessoPadrao, acao), Extensions.NotificationType.SUCCESS);
-        }
-        /// <summary>
-        /// Atenção! \n{0}
-        /// </summary>
-        public void AddNotificacaoAviso(string acao)
-        {
-            this.AddNotification(string.Format(MensgemAvisoPadrao, acao), Extensions.NotificationType.WARNING);
-        }
-        /// <summary>
-        /// Erro ao efetuar \n{0}!
-        /// </summary>
-        public void AddNotificacaoErro(string acao)
-        {
-            this.AddNotification(string.Format(MensgemErroPadrao, acao), Extensions.NotificationType.ERROR);
-        }
-
-        public void AddNotificacaoInformacao(string acao)
-        {
-            this.AddNotification(string.Format("{0}", acao), Extensions.NotificationType.INFO);
-        }
+      return View();
     }
+    /// <summary>
+    /// {0} realizado com sucesso!
+    /// </summary>
+    public void AddNotificacaoSucesso(string acao)
+    {
+      this.AddNotification(string.Format(MensgemSucessoPadrao, acao), Extensions.NotificationType.SUCCESS);
+    }
+    /// <summary>
+    /// Atenção! \n{0}
+    /// </summary>
+    public void AddNotificacaoAviso(string acao)
+    {
+      this.AddNotification(string.Format(MensgemAvisoPadrao, acao), Extensions.NotificationType.WARNING);
+    }
+    /// <summary>
+    /// Erro ao efetuar \n{0}!
+    /// </summary>
+    public void AddNotificacaoErro(string acao)
+    {
+      this.AddNotification(string.Format(MensgemErroPadrao, acao), Extensions.NotificationType.ERROR);
+    }
+
+    public void AddNotificacaoInformacao(string acao)
+    {
+      this.AddNotification(string.Format("{0}", acao), Extensions.NotificationType.INFO);
+    }
+  }
 }
