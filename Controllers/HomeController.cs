6f44ca591cb4415234d6c8e398e5485c94ed03ec
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-10-21 22:29:14 -0300

    consulta de animais para doação na home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using SejaHeroi.Models.Animal;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace SejaHeroi.Controllers
 {
-    public class HomeController : BaseController
+  public class HomeController : BaseController
+  {
+    public override ActionResult Index()
     {
-        public override ActionResult Index()
-        {  
-            return View();
-        }
+      List<AnimalViewModel> animaisDoacao = _SejaHeroiContextAnimalUtil.AnimaisDoacoes();
+      return View(animaisDoacao);
     }
+  }
 }
