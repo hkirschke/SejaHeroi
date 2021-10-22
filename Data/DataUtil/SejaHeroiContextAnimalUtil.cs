6b67c49143203547b0b116619f0f6d15885b82bc
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-10-21 22:31:03 -0300

    Trazer na consulta o status da vacina

--- Data/DataUtil/SejaHeroiContextAnimalUtil.cs
+++ Data/DataUtil/SejaHeroiContextAnimalUtil.cs
@@ -6,61 +6,64 @@ using System.Linq;
 
 namespace SejaHeroi.DataUtil
 {
-    public class SejaHeroiContextAnimalUtil : SejaHeroiContext
+  public class SejaHeroiContextAnimalUtil : SejaHeroiContext
+  {
+    public List<AnimalViewModel> AnimaisDoacoes()
     {
-        public List<AnimalViewModel> AnimaisDoacoes()
-        {
-            List<AnimalViewModel> animaisDoacao = (from animalList in Animals.ToList()
-                                                   where animalList.Situacao == Enums.Situacao.Disponivel
-                                                   select new AnimalViewModel
-                                                   {
-                                                       AnimalID = animalList.AnimalID,
-                                                       Nome = animalList.Nome,
-                                                       Idade = animalList.Idade,
-                                                       Situacao = animalList.Situacao,
-                                                       RacaAnimal = animalList.RacaAnimal,
-                                                       TamanhoAnimal = animalList.TamanhoAnimal,
-                                                       UsuarioID = animalList.UsuarioID,
-                                                       ByteFoto = animalList.Foto,
-                                                       Resenha = animalList.Resenha
-                                                   }).ToList();
-            return animaisDoacao;
-        }
-
-        public List<AnimalViewModel> GetAnimaisUsuario(Guid usuarioID)
-        {
-            List<AnimalViewModel> animaisDoacao = (from animalList in Animals.ToList()
-                                                   where animalList.UsuarioID == usuarioID
-                                                   select new AnimalViewModel
-                                                   {
-                                                       AnimalID = animalList.AnimalID,
-                                                       Nome = animalList.Nome,
-                                                       Idade = animalList.Idade,
-                                                       Situacao = animalList.Situacao,
-                                                       RacaAnimal = animalList.RacaAnimal,
-                                                       TamanhoAnimal = animalList.TamanhoAnimal,
-                                                       UsuarioID = animalList.UsuarioID
-                                                   }).ToList();
-            return animaisDoacao;
-        }
-
-        public List<AnimalViewModel> AnimaisDesaparecidos()
-        {
-            List<AnimalViewModel> animaisDoacao = (from animalList in Animals.ToList()
-                                                   where animalList.Situacao == Enums.Situacao.Desaparecido
-                                                   select new AnimalViewModel
-                                                   {
-                                                       AnimalID = animalList.AnimalID,
-                                                       Nome = animalList.Nome,
-                                                       Idade = animalList.Idade,
-                                                       Situacao = animalList.Situacao,
-                                                       RacaAnimal = animalList.RacaAnimal,
-                                                       TamanhoAnimal = animalList.TamanhoAnimal,
-                                                       UsuarioID = animalList.UsuarioID,
-                                                       ByteFoto = animalList.Foto,
-                                                       Resenha = animalList.Resenha
-                                                   }).ToList();
-            return animaisDoacao;
-        }
+      List<AnimalViewModel> animaisDoacao = (from animalList in Animals.ToList()
+                                             where animalList.Situacao == Enums.Situacao.Disponivel
+                                             select new AnimalViewModel
+                                             {
+                                               AnimalID = animalList.AnimalID,
+                                               Nome = animalList.Nome,
+                                               Idade = animalList.Idade,
+                                               Situacao = animalList.Situacao,
+                                               RacaAnimal = animalList.RacaAnimal,
+                                               TamanhoAnimal = animalList.TamanhoAnimal,
+                                               UsuarioID = animalList.UsuarioID,
+                                               ByteFoto = animalList.Foto,
+                                               Resenha = animalList.Resenha,
+                                               StatusVacina = animalList.StatusVacina
+                                             }).ToList();
+      return animaisDoacao;
     }
+
+    public List<AnimalViewModel> GetAnimaisUsuario(Guid usuarioID)
+    {
+      List<AnimalViewModel> animaisDoacao = (from animalList in Animals.ToList()
+                                             where animalList.UsuarioID == usuarioID
+                                             select new AnimalViewModel
+                                             {
+                                               AnimalID = animalList.AnimalID,
+                                               Nome = animalList.Nome,
+                                               Idade = animalList.Idade,
+                                               Situacao = animalList.Situacao,
+                                               RacaAnimal = animalList.RacaAnimal,
+                                               TamanhoAnimal = animalList.TamanhoAnimal,
+                                               UsuarioID = animalList.UsuarioID,
+                                               StatusVacina = animalList.StatusVacina
+                                             }).ToList();
+      return animaisDoacao;
+    }
+
+    public List<AnimalViewModel> AnimaisDesaparecidos()
+    {
+      List<AnimalViewModel> animaisDoacao = (from animalList in Animals.ToList()
+                                             where animalList.Situacao == Enums.Situacao.Desaparecido
+                                             select new AnimalViewModel
+                                             {
+                                               AnimalID = animalList.AnimalID,
+                                               Nome = animalList.Nome,
+                                               Idade = animalList.Idade,
+                                               Situacao = animalList.Situacao,
+                                               RacaAnimal = animalList.RacaAnimal,
+                                               TamanhoAnimal = animalList.TamanhoAnimal,
+                                               UsuarioID = animalList.UsuarioID,
+                                               ByteFoto = animalList.Foto,
+                                               Resenha = animalList.Resenha,
+                                               StatusVacina = animalList.StatusVacina
+                                             }).ToList();
+      return animaisDoacao;
+    }
+  }
 }
