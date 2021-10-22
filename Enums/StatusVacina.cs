66b4c25b3819cef51285bb7afb92636a532adc67
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-10-21 22:28:40 -0300

    enum para status vacina

---
+++ Enums/StatusVacina.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SejaHeroi.Enums
+{
+  public enum StatusVacina
+  {
+    [Display(Name = "Completa", Description = "Completa")]
+    Completa = 1,
+    [Display(Name = "Com pendência", Description = "Com pendência")]
+    Pendente = 0
+  }
+}
