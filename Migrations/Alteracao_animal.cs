3c09343db1a6f30046428c508750c856baa125ed
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-10-21 22:29:43 -0300

    migration de alteração do tipo da coluna de status vacina

---
+++ Migrations/Alteracao_animal.cs
@@ -0,0 +1,18 @@
+﻿namespace SejaHeroi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Alteracao_animal : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Animal", "StatusVacina", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Animal", "StatusVacina", c => c.Boolean(nullable: false));
+        }
+    }
+}
