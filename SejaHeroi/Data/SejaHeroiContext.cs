566e3d9108b351f5ab280215cb000ee9a90401a0
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-11-18 22:43:50 -0300

    Retirado método duplicado

--- SejaHeroi/Data/SejaHeroiContext.cs
+++ SejaHeroi/Data/SejaHeroiContext.cs
@@ -12,6 +12,5 @@ namespace SejaHeroi.Data
     public System.Data.Entity.DbSet<SejaHeroi.Models.FichaCastracao> FichaCastracaos { get; set; }
     public System.Data.Entity.DbSet<SejaHeroi.Models.EquipeVeterinario> EquipeVeterinarios { get; set; }
     public System.Data.Entity.DbSet<SejaHeroi.Models.Endereco> Enderecoes { get; set; } 
-    public System.Data.Entity.DbSet<SejaHeroi.Models.EquipeVeterinario> EquipeVeterinarios { get; set; }
   }
 }
