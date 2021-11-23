fd1df672fda597bb35af37766c90aaf973d75a35
hkirschke <33073460+hkirschke@users.noreply.github.com>
2021-11-22 21:40:13 -0300

    Create crud_endereco.cs

---
+++ SejaHeroi/Migrations/crud_endereco.cs
@@ -0,0 +1,32 @@
+﻿namespace SejaHeroi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class crud_endereco : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Endereco",
+                c => new
+                    {
+                        UsuarioID = c.Guid(nullable: false),
+                        Bairro = c.String(),
+                        Rua = c.String(),
+                        CEP = c.String(),
+                        Numero = c.Int(nullable: false),
+                        Telefone = c.String(),
+                        Celular = c.String(),
+                        Complemento = c.String(),
+                    })
+                .PrimaryKey(t => t.UsuarioID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Endereco");
+        }
+    }
+}
